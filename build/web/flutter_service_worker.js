3749556c9dfddd5fe84334f7d5e00273f0eedb9a
FelipeSBG14 <borges.felipe.felipe14@gmail.com>
2023-04-27 21:32:16 -0400

    realse

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -47,8 +47,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3ae37c682f743a124753ca13ed0f2412",
-"/": "3ae37c682f743a124753ca13ed0f2412",
+"index.html": "d6c296f9920ab32ec8816fa758463c44",
+"/": "d6c296f9920ab32ec8816fa758463c44",
 "main.dart.js": "f88b10aa1d343eae4ce7203185567dba",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
